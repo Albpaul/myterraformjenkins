5619274f19fba102f0bd4d3854500ba330d5e684
Paul.Umeojiako <paul.umeojiako@gmail.com>
2023-02-23 23:04:07 -0600

    more ec2 resources

--- main.tf
+++ main.tf
@@ -25,12 +25,30 @@ resource "aws_instance" "web_2" {
     Name = "jenkins-pipetutorial"
   }
 }
-resource "aws_instance" "web_3" {
+resource "aws_instance" "web_4" {
   ami           = "ami-0dfcb1ef8550277af"
   
   instance_type = "t3.micro"
 
   tags = {
-    Name = "jenkins-pipetutorial"
+    Name = "paul"
+  }
+}
+resource "aws_instance" "web_5" {
+  ami           = "ami-0dfcb1ef8550277af"
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "olequoa"
+  }
+}
+resource "aws_instance" "web_6" {
+  ami           = "ami-0dfcb1ef8550277af"
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "pauliwa"
   }
 }
