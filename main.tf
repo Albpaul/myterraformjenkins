7de59fbec78876c26c790eb674ee1890827f3e38
Paul.Umeojiako <paul.umeojiako@gmail.com>
2023-02-23 22:44:29 -0600

    fix terraform resource name

--- main.tf
+++ main.tf
@@ -7,3 +7,30 @@ resource "aws_instance" "web" {
     Name = "jenkins-pipetutorial"
   }
 }
+resource "aws_instance" "web_1" {
+  ami           = "ami-0dfcb1ef8550277af"
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "jenkins-pipetutorial"
+  }
+}
+resource "aws_instance" "web_2" {
+  ami           = "ami-0dfcb1ef8550277af"
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "jenkins-pipetutorial"
+  }
+}
+resource "aws_instance" "web_3" {
+  ami           = "ami-0dfcb1ef8550277af"
+  
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "jenkins-pipetutorial"
+  }
+}
